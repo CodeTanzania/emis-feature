d724178f1a096856846eced1316224e2bff67fe0
lykmapipo <lallyelias87@gmail.com>
2018-11-24 13:23:51 +0300

    Add changelog and build apidoc

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Feature",
+  "version": "1.0.0",
+  "description": "A representation of geographical feature of interest(i.e mapped physical element with their attributes in landscape e.g. administrative boundaries, roads, buildings etc) both natural and man made used in emergency(or disaster) management(or event).",
+  "title": "EMIS - Feature",
+  "url": "https://emis-feature.herokuapp.com/v1",
+  "sampleUrl": "https://emis-feature.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-24T10:22:22.442Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
