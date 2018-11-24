9814952fec9e37f66e4d22854757625c15a29bf7
lykmapipo <lallyelias87@gmail.com>
2018-11-24 12:52:24 +0300

    Improvement - Add seeds into examples - Simplify example app bootup - Improve tag generate to handle non array value set on tags

--- examples/app.js
+++ examples/app.js
@@ -21,37 +21,17 @@ const {
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 
-function boot() {
+Feature.seed((error, results) => {
 
-  async.waterfall([
-
-    function clearFeatures(next) {
-      Feature.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedFeatures(next) {
-      const features = Feature.fake(20);
-      Feature.insertMany(features, next);
-    }
-
-  ], function (error, results) {
-    console.log(error);
-
-    /* expose module info */
-    app.get('/', function (request, response) {
-      response.status(200);
-      response.json(info);
-    });
-
-    /* fire the app */
-    app.start(function (error, env) {
-      console.log(`visit http://0.0.0.0:${env.PORT}`);
-    });
-
+  /* expose module info */
+  app.get('/', function (request, response) {
+    response.status(200);
+    response.json(info);
   });
 
-}
+  /* fire the app */
+  app.start(function (error, env) {
+    console.log(`visit http://0.0.0.0:${env.PORT}`);
+  });
 
-boot();
+});
